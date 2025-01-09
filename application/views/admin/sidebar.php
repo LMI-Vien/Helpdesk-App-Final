7caa9b00da426b08e3c68a5ff1f6fa6a23ecb505
LMI-Luis <louise.lifestrong@gmail.com>
2025-01-09 15:08:48 +0800

    change the visibility

--- application/views/admin/sidebar.php
+++ application/views/admin/sidebar.php
@@ -23,6 +23,7 @@
                 </ul>
             </li>
 
+            <?php if ($this->session->userdata('login_data')['role'] !== 'L2'): ?>
             <li class="treeview <?= ($active_menu == 'closed_tickets_list') ? 'active' : ''; ?>">
                 <a href="#">
                     <i class="fa fa-ticket"></i> <span>Closed Tickets</span>
@@ -38,6 +39,7 @@
                     <li class="<?= ($active_menu == 'admin_list_tracc_request') ? 'active' : ''; ?>"><a href="<?= base_url(); ?>sys/admin/list/ticket/tracc_request_closed"><i class="fa fa-circle-o"></i> TRACC Request List</a></li>
                 </ul>
             </li>
+            <?php endif; ?>
 
             <!-- Try -->
             <li class="treeview <?= ($active_menu == 'customer_request_form_pdf' || $active_menu == 'customer_shipping_setup_pdf' || $active_menu == 'employee_request_form_pdf' || $active_menu == 'item_request_form_pdf' || $active_menu == 'supplier_request_form_pdf') ? 'active' : ''; ?>">
@@ -61,6 +63,7 @@
             </li>
             <!-- Try -->
 
+            <?php if ($this->session->userdata('login_data')['role'] !== 'L2'): ?>
             <li class="treeview <?= ($active_menu == 'system_administration' || $active_menu == 'users' || $active_menu == 'team' || $active_menu == 'cutoff') ? 'active' : ''; ?>">
                 <a href="#">
                     <i class="fa fa-gear"></i> <span>System Administration</span>
@@ -69,13 +72,13 @@
                     </span>
                 </a>
                 <ul class="treeview-menu">
-                    <?php if ($this->session->userdata('login_data')['role'] !== 'L2'): ?>
-                        <li class="<?= ($active_menu == 'users') ? 'active' : ''; ?>"><a href="<?= base_url(); ?>sys/admin/users"><i class="fa fa-users"></i> Users</a></li>
-                        <li class="<?= ($active_menu == 'team') ? 'active' : ''; ?>"><a href="<?= base_url(); ?>sys/admin/team"><i class="fa solid fa-user-plus"></i> Departments</a></li>
-                        <li class="<?= ($active_menu == 'cutoff') ? 'active' : ''; ?>"><a href="<?= base_url(); ?>sys/admin/cutoff"><i class="fa-solid fa-ban"></i>  Cutoff</a></li>
-                    <?php endif; ?>
+                    <li class="<?= ($active_menu == 'users') ? 'active' : ''; ?>"><a href="<?= base_url(); ?>sys/admin/users"><i class="fa fa-users"></i> Users</a></li>
+                    <li class="<?= ($active_menu == 'team') ? 'active' : ''; ?>"><a href="<?= base_url(); ?>sys/admin/team"><i class="fa solid fa-user-plus"></i> Departments</a></li>
+                    <li class="<?= ($active_menu == 'cutoff') ? 'active' : ''; ?>"><a href="<?= base_url(); ?>sys/admin/cutoff"><i class="fa-solid fa-ban"></i>  Cutoff</a></li>
                 </ul>
             </li>
+            <?php endif; ?>
+            
             <li class="<?= ($active_menu == 'print') ? 'active' : ''; ?>">
                 <a href="<?= base_url(); ?>sys/admin/print">
                     <i class="fa fa-print"></i> <span>Print Report</span>
