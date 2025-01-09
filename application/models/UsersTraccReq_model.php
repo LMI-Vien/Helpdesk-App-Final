6ffeb47955ce391bfe81f2e4524c1bb013f4ff4f
LMI-Luis <louise.lifestrong@gmail.com>
2025-01-09 10:48:44 +0800

    add logic on item request form - hindi makiktia ng lahat ang ticket number - logged in id === requestor id para sa form

--- application/models/UsersTraccReq_model.php
+++ application/models/UsersTraccReq_model.php
@@ -525,7 +525,7 @@ class UsersTraccReq_model extends CI_Model {
 	// 	return $query->result_array();
 	// }
 
-    public function get_item_request_form_from_tracc_req_mf_new_add() {
+    public function get_item_request_form_from_tracc_req_mf_new_add($user_id) {
         $this->db->select('tracc_req_mf_new_add.ticket_id');
         $this->db->from('tracc_req_mf_new_add');
         $this->db->join(
@@ -534,6 +534,7 @@ class UsersTraccReq_model extends CI_Model {
         );
         $this->db->where('tracc_req_mf_new_add.item', '1');
         $this->db->where('service_request_tracc_request.status !=', 'closed');
+		$this->db->where('service_request_tracc_request.requested_by_id', $user_id);
         $query = $this->db->get();
         return $query->result_array();
     }
